36b0c1d231a6cc1fcdac73c3cf6f6f5fa7392853
islomabdulakhatov <islomabdulakhatov@gmail.com>
2023-02-28 08:40:20 +0500

    Server Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { API_REQUEST } from 'src/services/api.service'
 import { Header } from './components'
 
-export default function Home() {
+export default function Home(props:HomeProps): JSX.Element {
+  console.log(props);
+  
+
+  useEffect(() => {
+    fetch(API_REQUEST.trending).then(res => res.json()).then(data => console.log(data))
+  }, [])
+  
+
   return (
     <div className='relative h-[200vh]'>
       <Head>
@@ -22,4 +33,27 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const trending = await fetch(API_REQUEST.trending).then(res => res.json())
+
+  // if (trending.results.length) {
+  //   return {
+  //     redirect: {
+  //       destination: '/account'
+  //     }
+  //   }
+  // }
+
+  return {
+    props: {
+      message: 'Message from SSR',
+      trending: trending
+    }
+  }
+}
+
+interface HomeProps {
+  message:any
 }
