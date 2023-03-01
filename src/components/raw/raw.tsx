3117a4bbbfe9593c21e3713e4a4ae2837afcadab
islomabdulakhatov <islomabdulakhatov@gmail.com>
2023-03-01 07:19:13 +0500

    carousel

--- src/components/raw/raw.tsx
+++ src/components/raw/raw.tsx
@@ -1,23 +1,58 @@
 import { RawProps } from "./raw.props";
 import { AiFillCaretRight, AiFillCaretLeft } from "react-icons/ai";
 import Thumbnail from "../thumbnail/thumbnail";
+import { useRef, useState } from "react";
 
 const Raw = ({ title, movies }: RawProps) => {
+  const [moved, setMoved] = useState<Boolean>(false);
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = (direction: "left" | "right") => {
+    setMoved(true);
+
+    if (carouselRef.current) {
+      const { scrollLeft, clientWidth } = carouselRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      carouselRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+
+      if (direction === "left" && scrollTo === 0) {
+        setMoved(false);
+      }
+    }
+  };
+
   return (
-    <div className="h-[500px] space-y-1 md:space-y-2">
+    <div className="space-y-1 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm md:text-2xl font-semibold text-[#e5e5e5] hover:text-white transition duration-200">
         {title}
       </h2>
       {/* Carousel */}
       <div className="group relative md:ml-2">
-        <AiFillCaretLeft className="absolute top-0 bottom-0 left-2 z-40 m-auto h-6 w-6 cursor-pointer opacity-0 group-hover:opacity-100 transition duration-200 hover:scale-125" />
+        <AiFillCaretLeft
+          onClick={() => handleClick("left")}
+          className={`${
+            !moved && "hidden"
+          } absolute top-0 bottom-0 left-2 z-40 m-auto h-6 w-6 cursor-pointer opacity-0 group-hover:opacity-100 transition duration-200 hover:scale-125`}
+        />
         {/* Thumbnail */}
-        <div className="flex items-center space-x-0.5 md:space-x-2 overflow-x-scroll">
+
+        <div
+          ref={carouselRef}
+          className="flex scrollbar-hide items-center overflow-hidden space-x-1 md:space-x-4 overflow-x-scroll">
           {movies?.map((movie) => (
             <Thumbnail key={movie?.id} movie={movie} />
           ))}
         </div>
-        <AiFillCaretRight className="absolute top-0 bottom-0 right-2 z-40 m-auto h-6 w-6 cursor-pointer  opacity-0 group-hover:opacity-100 transition duration-200 hover:scale-125" />
+
+        <AiFillCaretRight
+          onClick={() => handleClick("right")}
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-6 w-6 cursor-pointer  opacity-0 group-hover:opacity-100 transition duration-200 hover:scale-125`}
+        />
       </div>
     </div>
   );
