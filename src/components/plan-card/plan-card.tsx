f0b3423bf0b5a7414845b9326f402fa5b2824708
islomabdulakhatov <islomabdulakhatov@gmail.com>
2023-03-06 14:51:18 +0500

    subscription

--- src/components/plan-card/plan-card.tsx
+++ src/components/plan-card/plan-card.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { AiOutlineHourglass, AiOutlineVideoCameraAdd } from "react-icons/ai";
 import { RiVipCrown2Line } from "react-icons/ri";
+import { AuthContext } from "src/context/auth.context";
 import { PlanCardProps } from "./plan-card.props";
 
 const PlanCard = ({ product }: PlanCardProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { user } = useContext(AuthContext);
+
+  const onSubmitSubscription = async (priceId: string) => {
+    setIsLoading(true);
+    const payload = { email: user?.email, priceId };
+
+    try {
+      const response = await fetch("/api/subscription", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+      const data = await response.json();
+      window.open(data.subscription.url);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(true);
+    }
+  };
+
   return (
     <div className="max-w-sm cursor-pointer bg-white/20 px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
       <h3 className="mb-3 text-xl font-bold text-[#E10856]">{product.name}</h3>
@@ -24,9 +46,10 @@ const PlanCard = ({ product }: PlanCardProps) => {
       </div>
       <div className="border-[1px] border-white/20 mt-4" />
       <button
-        // onClick={() => onSubmitSubscription(product.default_price.id)}
-        className="mt-4 w-full bg-[#E10856] py-4 rounded hover:opacity-80 font-semibold">
-        {"BUY PLAN"}
+        onClick={() => onSubmitSubscription(product.default_price.id)}
+        className="mt-4 w-full bg-[#E10856] py-4 rounded hover:opacity-80 font-semibold"
+        disabled={isLoading}>
+        {isLoading ? "Loading..." : "BUY PLAN"}
       </button>
       <div className="my-4 flex flex-col space-y-2">
         {product.metadata.adv.split(", ").map((c, id) => (
